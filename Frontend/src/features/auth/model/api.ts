cb9e2b52775568a310684a3617f6779609137032
Shaqs0 <hhgfhhhggb@gmail.com>
2025-07-28 14:12:26 +0300

    more updates

--- Frontend/src/features/auth/model/api.ts
+++
@@ -1,6 +0,0 @@
-// import { api } from '@/shared/api/base';
-
-// export async function loginApi(credentials: {email: string; password: string}) {
-//   const { data } = await api.post('/auth/login', credentials);
-//   return data;
-// }
--- Frontend/src/pages/MainPage/MainPage.tsx
+++ Frontend/src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,3 @@
-
-import { useLoaderData } from '@tanstack/react-router';
-
 export function MainPage() {
-	const data = useLoaderData({ from: '/main' }); 
 
-	return (
-		<div>
-			<h2>Main Page</h2>
-			<p>Data: {data.message}</p>
-		</div>
-	);
 }

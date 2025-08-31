7f8e03968f69574ad273c6edb945478ea862099a
Maksim Beloglazov <maksimbel20170@gmail.com>
2025-08-31 20:07:38 +0300

    Исправление проблем

--- Backend/ChatFlow/Configuration/VaultConfigurationExtensions.cs
+++ Backend/ChatFlow/Configuration/VaultConfigurationExtensions.cs
@@ -12,7 +12,6 @@ public static class VaultConfigurationExtensions
         var address = config.GetSection("Vault:Address").Value!;
         var roleId = config.GetSection("Vault:RoleId").Value!;
         var secretId = config.GetSection("Vault:SecretId").Value!;
-        Console.WriteLine($"Address: {address} || RoleId: {roleId} || SecretId: {secretId}");
 
         var authMethod = new AppRoleAuthMethodInfo(roleId, secretId);
         var vaultClientSettings = new VaultClientSettings(address, authMethod);
